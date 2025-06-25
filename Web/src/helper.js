774a9385e549c44020214de114359f14bce430cc
DevPatata <anavaro.sala@gmail.com>
2025-06-25 16:56:31 +0200

    unity branch

--- Web/src/helper.js
+++
@@ -1,8 +0,0 @@
-function CollisionDetector(objeto1, objeto2){
-    if(objeto1.x >= objeto2.x + objeto2.width ||
-        objeto1.x + objeto1.width <= objeto2.x ||
-        objeto1.y >= objeto2.y + objeto2.height||
-        objeto1.y + objeto1.height <= objeto2.y)
-        return false;
-    else return true;
-}
