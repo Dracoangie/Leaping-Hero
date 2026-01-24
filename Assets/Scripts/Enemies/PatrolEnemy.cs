eec3a628ca20f3a5d56f24ae8ab0be355637521d
DevPatata <anavaro.sala@gmail.com>
2026-01-24 02:54:51 +0100

    First_level Enemies

--- Assets/Scripts/Enemies/PatrolEnemy.cs
+++ Assets/Scripts/Enemies/PatrolEnemy.cs
@@ -1,47 +1,65 @@
 using UnityEngine;
 
+
 public class PatrolEnemy : Enemy
 {
     public float patrolSpeed = 2f;
-    public Vector3 patrolPointA;
-    public Vector3 patrolPointB;
+    public Transform patrolPointA;
+    public Transform patrolPointB;
 
     private Vector3 targetPoint;
+
+    private enum MovementType
+    {
+        move,
+        noMove
+    }
+
+    [SerializeField]
+    private MovementType movementType = MovementType.move;
 
     protected override void Start()
     {
         base.Start();
 
-        transform.position = patrolPointA;
-        targetPoint = patrolPointA;
-        if (patrolPointA == Vector3.zero && patrolPointB == Vector3.zero)
+        transform.position = patrolPointA.position;
+        targetPoint = patrolPointA.position;
+        if (patrolPointA.position == Vector3.zero && patrolPointB.position == Vector3.zero)
         {
-            patrolPointA = transform.position;
-            patrolPointB = transform.position + new Vector3(5, 0, 0);
-            targetPoint = patrolPointB;
+            patrolPointA.position = transform.position;
+            patrolPointB.position = transform.position + new Vector3(5, 0, 0);
+            targetPoint = patrolPointB.position;
         }
     }
 
     protected override void HandleIdle()
     {
         base.HandleIdle();
-        Vector3 direction = (targetPoint - transform.position).normalized;
-        rb.linearVelocity = direction * patrolSpeed;
 
-        if (direction.x > 0.01f)
+        switch(movementType)
         {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (direction.x < -0.01f)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
-        if (Vector2.Distance(transform.position, targetPoint) < 0.2f)
-        {
-            if (targetPoint == patrolPointA)
-                targetPoint = patrolPointB;
-            else
-                targetPoint = patrolPointA;
+            case MovementType.move:
+                Vector3 direction = (targetPoint - transform.position).normalized;
+                rb.linearVelocity = direction * patrolSpeed;
+
+                if (direction.x > 0.01f)
+                {
+                    transform.localScale = new Vector3(1, 1, 1);
+                }
+                else if (direction.x < -0.01f)
+                {
+                    transform.localScale = new Vector3(-1, 1, 1);
+                }
+                if (Vector2.Distance(transform.position, targetPoint) < 0.2f)
+                {
+                    if (targetPoint == patrolPointA.position)
+                        targetPoint = patrolPointB.position;
+                    else
+                        targetPoint = patrolPointA.position;
+                }
+                break;
+            case MovementType.noMove:
+                break;
         }
     }
 
